2caadd212e044ec32d2f33aec0063ed2edf5d7e4
Ansel Sulejmani <anselsulejmani826@gmail.com>
2025-09-16 21:03:31 -0600

    Create vulkan instance and application object

---
+++ VulkanTest/VulkanTest/HelloTriangleApplication.cpp
@@ -0,0 +1,94 @@
+#include "HelloTriangleApplication.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+void HelloTriangleApplication::run() {
+	this->initWindow();
+	this->initVulkan();
+	this->mainLoop();
+	this->cleanup();
+}
+
+void HelloTriangleApplication::createInstance() {
+	// specifying some extra information to the driver to optimize our application
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello triangle woah";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0); // this is actually so useful holy shit
+	appInfo.pEngineName = "David Engine (biblically acccurate)";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+	// we need to pass throw global extensions for window managementn glfw makes this a bit simpler for us
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	// keep this empty for right now
+	createInfo.enabledLayerCount = 0;
+
+	// before we create the instance we first want to check to see what extensions are available to us
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+	std::cout << "Accepted extensions" << std::endl;
+	for (const auto &extension : extensions) { // remember that c++ has for-each loops now
+		std::cout << "\t" << extension.extensionName << '\n'; // doing '\n' is actually better than endl since endl also flushes the stream
+	}
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) { // we can make this an error check function (hellloooooooo)
+		throw std::runtime_error("failed to create instance!");
+	}
+
+}
+
+void HelloTriangleApplication::initVulkan() {
+	// making an instance, this is connection between vulkan and this application
+	createInstance();
+
+}
+
+
+void HelloTriangleApplication::mainLoop() {
+	// have an event loop that continues until eventually the window closes or an error is thrown
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); // there should be a way to also send out this signal ourselves without clicking the "x" button
+	}
+
+
+
+
+}
+
+void HelloTriangleApplication::cleanup() {
+	// destory the window and deallocate the memory
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	// make sure you destory the instance of vulkan that was created
+	vkDestroyInstance(instance, nullptr); // no callbacks here but if we did have one we'd have to but in in this function for de allocation
+}
+
+// pretty self explanatory what this does
+void HelloTriangleApplication::initWindow() {
+	// first we initialize the glfw library and then immeditaely tell it not to you use opengl context
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "this is a title", nullptr, nullptr);
+
+
+}
---
+++ VulkanTest/VulkanTest/HelloTriangleApplication.h
@@ -0,0 +1,28 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run();
+
+private:
+    
+    GLFWwindow* window;
+    
+    void initWindow();
+    
+    void initVulkan();
+
+    void mainLoop();
+
+    void cleanup();
+
+    VkInstance instance;
+
+    void createInstance();
+};
